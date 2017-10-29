7643e7e412522aab3d61d1aeb1830b7775bc2dca
Erik Golden <erikgolden7@gmail.com>
2017-10-28 21:25:03 -0500

    mongo db and more oauth

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+const mongoose = require("mongoose");
+const keys = require("./config/keys.js");
+require("./models/User.js");
+require("./services/passport.js");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-
-// Google Auth
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback"
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log(accessToken);
-			console.log(refreshToken);
-			console.log(profile);
-		}
-	)
-);
-
-app.get(
-	"/auth/google",
-	passport.authenticate("google", {
-		scope: ["profile", "email"]
-	})
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes.js")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("listening on port:", PORT));
